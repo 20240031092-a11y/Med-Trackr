8e9c40778c74ed09a7a0aad416c3b2b77cb4939f
20240031092-a11y <20240031092@my.xu.edu.ph>
2026-04-24 06:03:54 +0800

    1.0

---
+++ config/footer.php
@@ -0,0 +1,12 @@
+<footer class="main-footer fixed-bottom">
+    <strong>Copyright &copy; <?php echo date('Y');?> 
+    <a href="./">Med's TRCKR</a>.</strong> All rights reserved.
+    <div class="float-right d-sm-block">
+      MAR Version 2.0
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
